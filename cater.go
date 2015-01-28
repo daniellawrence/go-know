ac488ec7b4de9212ea6217222aeb805f500c7463
Daniel Lawrence <dannyla@linux.com>
2015-01-29 00:31:05 +1100

    Getting ready to fake files from duplicate hashes

--- cater.go
+++ cater.go
@@ -19,9 +19,51 @@ type FileStat struct {
 }
 
 
+// Make the Hostnames an array,
+// Store every hostname that matches the hash
+// Fake the file for each to the stdout buffer
+type Hash struct {
+	Hostnames  string
+	Contents   []byte
+}
+
+
+func StringInArray(a string, list map[string]Hash) bool {
+	for q, _ := range list {
+		if q == a {
+			return true
+		}
+	}
+	return false
+}
+
 
 func cater(path string) bytes.Buffer {
-	var response bytes.Buffer;
+	var response bytes.Buffer
+	knownHashes := grab(path)
+
+	for _, hash := range knownHashes {
+
+		var b bytes.Buffer
+		b.Write([]byte(hash.Contents))
+
+		gr, _ := gzip.NewReader(&b)
+		defer gr.Close()
+		plaintext, _ := ioutil.ReadAll(gr)
+
+		for _, a := range strings.Split(string(plaintext), "\n") {
+			line := fmt.Sprintf("%s:%s\n", hash.Hostnames, a)
+			response.Write([]byte(line))
+		}
+	}
+	return response
+
+}
+
+
+func grab(path string) map[string]Hash {
+	var file_contents_gzipped string
+	knownHashes := make(map[string]Hash)
 
 	fmt.Printf("search path: %s\n", path)
 	conn, _ := redis.Dial("tcp", ":6379")
@@ -34,22 +76,19 @@ func cater(path string) bytes.Buffer {
 		y := []byte(reply)
 
 		json.Unmarshal(y, &fs)
-		file_contents_gzipped, _ := redis.String(conn.Do("GET", fs.Hash))
 
-		var b bytes.Buffer
-		b.Write([]byte(file_contents_gzipped))
-
-		gr, _ := gzip.NewReader(&b)
-		defer gr.Close()
-		plaintext, _ := ioutil.ReadAll(gr)
-
-		for _, a := range strings.Split(string(plaintext), "\n") {
-			line := fmt.Sprintf("%s:%s\n", fs.Hostname, a)
-			response.Write([]byte(line))
+		// Check if we already have this hash, before wasting redis time
+		if ! StringInArray(fs.Hash, knownHashes) {
+			file_contents_gzipped, _ = redis.String(conn.Do("GET", fs.Hash))
+			knownHashes[fs.Hash] = Hash{
+				// Need to switch this to an array of hostnames
+				// to fake the output and save lookups, ect
+				Hostnames: fs.Hostname,
+				Contents: []byte(file_contents_gzipped),
+			}
 		}
 	}
-	return response
-	
+	return  knownHashes
 }
 
 func main() {
